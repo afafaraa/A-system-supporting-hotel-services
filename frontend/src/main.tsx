c8484c478e639c28737ccbca40ad5b02e392bedb
Konrad Tendaj <konradtendaj@student.agh.edu.pl>
2025-03-12 15:19:44 +0100

    application.properties replaced with application.yml, Dockerfile for backend, frontend log-in test in LoginForm

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import LoginForm from "./components/LoginForm.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <span>Hello, Vite + React!</span>
+      <LoginForm/>
   </StrictMode>,
 )
