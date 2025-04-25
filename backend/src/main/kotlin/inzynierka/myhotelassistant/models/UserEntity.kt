aa4b9e8f8fbf67393fc9d2c2877d8299bda5a9de
Konrad Tendaj <konradtendaj@student.agh.edu.pl>
2025-04-25 19:33:29 +0200

    Indexed fields in UserEntity

--- backend/src/main/kotlin/inzynierka/myhotelassistant/models/UserEntity.kt
+++ backend/src/main/kotlin/inzynierka/myhotelassistant/models/UserEntity.kt
@@ -4,21 +4,32 @@ import inzynierka.myhotelassistant.models.room.RoomEntity
 import inzynierka.myhotelassistant.models.order.OrderEntity
 import inzynierka.myhotelassistant.models.notification.NotificationEntity
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
 import java.time.Instant
 
 @Document(collection = "users")
 data class UserEntity(
+
     @Id val id: String? = null,
-    val username: String,
-    var password: String,
+
+    var role: Role = Role.GUEST,
+
+    @Indexed(unique = true)
     var email: String,
+
+    @Indexed(unique = true)
+    val username: String,
+
+    var password: String,
+
     val name: String? = null,
     val surname: String? = null,
+
     val room: RoomEntity? = null,
     val checkInDate: Instant? = null,
     val checkOutDate: Instant? = null,
-    var role: Role = Role.GUEST,
+
     val orders: MutableList<OrderEntity> = mutableListOf(),
     val notifications: MutableList<NotificationEntity> = mutableListOf(),
 )
