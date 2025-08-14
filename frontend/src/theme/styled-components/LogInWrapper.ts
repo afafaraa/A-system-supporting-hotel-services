3a443f22c9dfe4b8b8fea44d6865f0d5968b2585
Konrad Tendaj <konradtendaj@student.agh.edu.pl>
2025-08-14 17:23:42 +0200

    Language switcher fitted into the login page

--- frontend/src/theme/styled-components/LogInWrapper.ts
+++ frontend/src/theme/styled-components/LogInWrapper.ts
@@ -4,7 +4,7 @@ export const LogInWrapper = styled('div')(
   ({ theme }) => ({
     backgroundColor: theme.palette.custom1.primary,
     padding: '40px 30px',
-    borderRadius: '25px',
+    borderRadius: '15px',
     boxShadow: `0px 0px 15px -1px ${theme.palette.custom3.contrastText}`,
   })
 );
