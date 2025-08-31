f52dda22dca5c3bfee8558c22213a276c9b8b069
Konrad Tendaj <konradtendaj@student.agh.edu.pl>
2025-08-31 21:51:19 +0200

    Rate service dialog with mui rating

--- frontend/src/pages/guest/available-services/StarRating.tsx
+++ frontend/src/pages/guest/available-services/StarRating.tsx
@@ -1,9 +1,7 @@
-import { useMediaQuery } from "@mui/material";
 import {Rating as RatingType} from "./AvailableServiceCard.tsx";
 import Rating from "@mui/material/Rating";
 
 const StarRating = ({ rating }: {rating: RatingType[]}) => {
-  const isXs = useMediaQuery(theme => theme.breakpoints.only("xs"));
 
   const calcRating = () => {
     if (rating.length === 0) return 0;
@@ -12,7 +10,7 @@ const StarRating = ({ rating }: {rating: RatingType[]}) => {
     return total / rating.length;
   }
 
-  return <Rating readOnly defaultValue={calcRating()} precision={0.5} size={isXs ? "large" : "medium"} />
+  return <Rating readOnly defaultValue={calcRating()} precision={0.5} sx={{fontSize: '150%'}} />
 };
 
 export default StarRating;
