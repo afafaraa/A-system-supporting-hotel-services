c9c7a4cbfd465a358193b9b20ad28f8c413e9096
Aleksandra Fafara <aleksandra.fafara11@gmail.com>
2025-11-24 16:20:33 +0100

    build fix

--- backend/src/test/kotlin/inzynierka/myhotelassistant/utils/email/EmailSenderTest.kt
+++ backend/src/test/kotlin/inzynierka/myhotelassistant/utils/email/EmailSenderTest.kt
@@ -28,7 +28,6 @@ class EmailSenderTest {
         appProperties =
             AppProperties().apply {
                 frontend.url = "http://localhost:5173"
-                frontend.port = "5173"
             }
         emailSender = EmailSender(mailSender, appProperties, null)
     }
