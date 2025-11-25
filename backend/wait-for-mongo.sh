936040ef773bcb84fe1663f2293f269ebc9c8d22
Aleksandra Fafara <aleksandra.fafara11@gmail.com>
2025-11-25 12:41:07 +0100

    fix wait for backend to run on railway

--- backend/wait-for-mongo.sh
+++ backend/wait-for-mongo.sh
@@ -1,14 +1,6 @@
-#!/bin/sh
-
-MONGO_HOST=${MONGOHOST:-mongo}
-MONGO_PORT=${MONGOPORT:-27017}
-
-echo "Waiting for MongoDB at $MONGO_HOST:$MONGO_PORT..."
-
+MONGO_HOST=${MONGO_HOST:-mongo}
+MONGO_PORT=${MONGO_PORT:-27017}
 until nc -z "$MONGO_HOST" "$MONGO_PORT"; do
-  echo "MongoDB not available yet..."
   sleep 2
 done
-
-echo "MongoDB is up!"
 exec "$@"
